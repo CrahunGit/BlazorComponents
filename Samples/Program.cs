8d07698ca56b029dae2e7e7f5c35154161afcee0
Tomás López <tlopez@facephi.com>
2021-03-18 16:47:08 +0100

    Fixed

--- Samples/Program.cs
+++ Samples/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Modal;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +19,7 @@ namespace Samples
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddBlazoredModal();
 
             await builder.Build().RunAsync();
         }
