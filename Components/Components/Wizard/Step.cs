be49615779cc8af75129130c277eff0d2f5a767a
Tomás López <tlopez@facephi.com>
2021-03-18 15:16:35 +0100

    Created Wizard component for blazor

---
+++ Components/Components/Wizard/Step.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Components.Components.Wizard
+{
+    public class Step: ComponentBase
+    {
+        [Parameter]
+        public string Name { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [CascadingParameter(Name = "Parent")]
+        public Wizard Parent { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Parent.AddStep(this);
+        }
+    }
+}
